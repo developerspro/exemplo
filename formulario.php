09325c33036b623bdc5fa3bc0c065f8b94d73064
Jeferson Ferreira <jeferson.oferreira@etec.sp.gov.br>
2024-04-04 20:36:49 -0300

    arrays

---
+++ formulario.php
@@ -0,0 +1,10 @@
+<pre>
+<form action="acao.php" method="post">
+<input type="text" name="funcionario" id="">
+    <input type="text" name="" id="">
+    <input type="text" name="" id="">
+    <input type="text" name="cadastro[]" id="">
+    <input type="text" name="cadastro[]" id="">
+    <input type="submit" value="enviar">
+</form>
+</pre>
