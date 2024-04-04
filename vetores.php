f217abac1190f53426915650aae42e342b4dfc58
Jeferson Ferreira <jeferson.oferreira@etec.sp.gov.br>
2024-04-04 20:17:14 -0300

    arrays

--- vetores.php
+++ vetores.php
@@ -1,11 +1,27 @@
 <?php
 echo "<pre>";
-$lista[10] = 10;
-$lista[] = 11; 
-$lista[] = 12;
-$lista[] = "nome";
+$lista["A"] = 10;
+$lista["B"] = 11; 
+$lista["C"] = 12;
 $lista[] = 13;
+$lista[] = 14;
+$lista["texto"] =15;
+$lista[] = 16;
 
+
+$lista["alunos"] = array("matricula"=>1,"email"=>"aluno@eteck");
+$lista["materias"] = array("DES I","PWII");
+echo $lista["alunos"]["matricula"];
+var_dump($lista);
+for ($i=0; $i < count($lista); $i++) { 
+   
+    echo "Posição;".$i." Valor:".$lista[$i]."<br>";
+}
+
+foreach ($lista as $valor){
+    echo "Valor:".$valor."<br>";
+}
+echo "<br><br>";
 var_dump($lista);
 
 $lista1 = array();
@@ -13,5 +29,7 @@ $lista1 = array();
 var_dump($lista1);
 
 $lista2 = [1,2,3,"teste",5];
+var_dump($lista2);
+unset($lista2[3]);
 $lista2[] = 20;
 var_dump($lista2);
